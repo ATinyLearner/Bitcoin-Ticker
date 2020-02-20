68761b5955537576ab532f15df0a402c0d8e89f2
ATinyLearner <avik423@outlook.com>
2020-02-20 14:07:53 +0530

    BetaV1

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Bitcoin Ticker 🤑
 
-</br>This is a App where we can check bitcoin value in other currency.
-</br># Changelog
+</br>This is a App where we can check bitcoin value in other currency.</br>
+# Changelog
 </br>--------------------</br>
 Initial Release</br>
 --------------------</br>
 Added Some basic features</br>
 App only runs but not usable now</br>
-
+----------------------------</br>
+BetaV1</br>
+----------------------------</br>
+App is now working with some cyptoCurrency rate along with all normal currency
+BTC,ETH and LTC is working with all money currency(i.e USD,INR,GBP etc)
+App is usable now
--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,11 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import './price_screen.dart';
+
+const apiKey = '94FBD943-61A4-457A-9028-D4CA9A5DD37A';
+const coinApiUrl = 'https://rest.coinapi.io/v1/exchangerate';
+
+int indexNumber = 0;
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +36,25 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  PriceScreen indexCurrency = PriceScreen();
+  Future<dynamic> getCoinData(String selectedCurrency) async {
+    //String selectedCurrency = currenciesList[indexCurrency.getIndex()];
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestedURL =
+          '$coinApiUrl/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(requestedURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        var lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+
+    return cryptoPrices;
+  }
+}
